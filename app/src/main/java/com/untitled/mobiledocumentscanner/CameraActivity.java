f3b5a2b7d4dd0b77e7150bb025c3a9e2f324af83
Amber <amber.diskin@sky.com>
2017-03-21 19:38:28 +0000

    Added permissions to manifest.

--- app/src/main/java/com/untitled/mobiledocumentscanner/CameraActivity.java
+++ app/src/main/java/com/untitled/mobiledocumentscanner/CameraActivity.java
@@ -1,7 +1,7 @@
 package com.untitled.mobiledocumentscanner;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
 
 public class CameraActivity extends AppCompatActivity {
 
